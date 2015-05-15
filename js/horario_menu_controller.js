1a54bf52731040dee03d88c9f8c49ee0cefe655f
zeorsi <luismarquezcarpintero@gmail.com>
2015-05-15 12:11:31 +0200

    Controlador menú horario
    Se me había olvidado modificarlo.

--- js/horario_menu_controller.js
+++ js/horario_menu_controller.js
@@ -1,9 +1,5 @@
 starter.controller('HorarioMenuCtrl', function($scope, $rootScope, $stateParams, $http) {
     
-    $scope.documento = $stateParams.documento;
 
-    $http.get($rootScope.ajaxURL+"horario2.php?dni="+$rootScope.dniDocente).success(function(data){
-        $scope.horario=data;
-    });
 	   
 });
